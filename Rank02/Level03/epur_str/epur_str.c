613462efe47186c2710e62d0d5b13aa21cadf212
mcatalan15 <marc.prat.2001@gmail.com>
2023-09-26 11:07:01 +0200

    creating folders, adding ulstr, adding do_op, adding epur_str

---
+++ Rank02/Level03/epur_str/epur_str.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+	int i;
+	int flg;
+
+	if (argc == 2)
+	{
+		i = 0;
+		while (argv[1][i] == ' ' || argv[1][i] == '\t')
+			i += 1;
+		while (argv[1][i])
+		{
+			if (argv[1][i] == ' ' || argv[1][i] == '\t')
+				flg = 1;
+			if (!(argv[1][i] == ' ' || argv[1][i] == '\t'))
+			{
+				if (flg)
+					write(1, " ", 1);
+				flg = 0;
+				write(1, &argv[1][i], 1);
+				if (argv[1][i + 1] == '\0')
+				{
+					write(1, "\n",1);
+					return (0);
+				}
+			}
+			i += 1;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
