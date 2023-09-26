5d44d271ac38d546d0860618cf2614b507b4e85c
mcatalan15 <marc.prat.2001@gmail.com>
2023-09-26 21:21:06 +0200

    adding Level01/rotone Level02/max Level03/expand_str Level04/rosting

---
+++ Rank02/Level03/expand_str/expand__str.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+
+int checker(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\0')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int main(int ac, char **av)
+{
+	int i;
+
+	i = 0;
+	if (ac == 2)
+	{
+		while (av[1][i] == ' ' || av[1][i] == '\t')
+			i++;
+		while (av[1][i])
+		{
+			if (av[1][i] != ' ' || av[1][i] != '\t')
+			{
+				write(1, &av[1][i], 1);
+				if (av[1][i + 1] == ' ' || av[1][i + 1] == '\t')
+				{
+					if (checker(&av[1][i + 1]) == 1)
+						write(1, "   ", 3);
+				}
+				i++;
+			}
+			while (av[1][i] == ' ' || av[1][i] == '\t')
+				i++;
+		}
+	}
+	write(1, "\n", 1);
+}
