26cc3b6fd93ab5473ee9dc4deffed3f45825cbb5
mcatalan15 <marc.prat.2001@gmail.com>
2023-09-28 20:15:46 +0200

    adding Level02/ft_strdup Level02/paramsum Level03/epur_str Level03/ft_rrange

--- Rank02/Level03/epur_str/epur_str.c
+++ Rank02/Level03/epur_str/epur_str.c
@@ -1,34 +1,28 @@
 #include <unistd.h>
 
-int main(int argc, char const *argv[])
+int main(int ac, char **av)
 {
-	int i;
-	int flg;
+	int i = 0;
+	int k = 0;
 
-	if (argc == 2)
+	if (ac == 2)
 	{
 		i = 0;
-		while (argv[1][i] == ' ' || argv[1][i] == '\t')
-			i += 1;
-		while (argv[1][i])
+		while (av[1][i] == ' ' || av[1][i] == '\t')
+			i++;
+		while (av[1][i])
 		{
-			if (argv[1][i] == ' ' || argv[1][i] == '\t')
-				flg = 1;
-			if (!(argv[1][i] == ' ' || argv[1][i] == '\t'))
+			if (av[1][i] == ' ' || av[1][i] == '\t')
+				k = 1;
+			if (!(av[1][i] == ' ' || av[1][i] == '\t')) // if (av[1][i] != ' ' && av[1][i] != '\t')  -->  It can also be written like this
 			{
-				if (flg)
+				if (k)
 					write(1, " ", 1);
-				flg = 0;
-				write(1, &argv[1][i], 1);
-				if (argv[1][i + 1] == '\0')
-				{
-					write(1, "\n",1);
-					return (0);
-				}
+				k = 0;
+				write(1, &av[1][i], 1);
 			}
-			i += 1;
+			i++;
 		}
 	}
 	write(1, "\n", 1);
-	return (0);
 }
