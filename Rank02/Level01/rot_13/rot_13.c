eeafd46f313e831c0eef665f59c348b55905cfdd
mcatalan15 <marc.prat.2001@gmail.com>
2023-11-10 13:18:28 +0100

    adding .txt on rot_13 and ft_strlen

--- Rank02/Level01/rot_13/rot_13.c
+++ Rank02/Level01/rot_13/rot_13.c
@@ -8,7 +8,7 @@ int main(int ac, char **av)
 	{
 		while (av[1][i])
 		{
-			if ((av[1][i] >= 'a' && av[1][i] <= 'm') || (av[1][i]) >= 'A' && av[1][i] <= 'M')
+			if ((av[1][i] >= 'a' && av[1][i] <= 'm') || (av[1][i] >= 'A' && av[1][i] <= 'M'))
 				av[1][i] += 13;
 			else if ((av[1][i] >= 'n' && av[1][i] <= 'z') || (av[1][i] >= 'N' && av[1][i] <= 'Z'))
 				av[1][i] -= 13;
