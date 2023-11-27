db32f06e5b16b5611e063b1c0ed34c93055224a1
mcatalan15 <marc.prat.2001@gmail.com>
2023-11-27 18:49:25 +0100

    changing rstr_capitalizer

--- Rank02/Level03/rstr_capitalizer/rstr_capitalizer.c
+++ Rank02/Level03/rstr_capitalizer/rstr_capitalizer.c
@@ -10,7 +10,8 @@ void rstr_capitalizer(char *str)
 			str[i] += 32;
 		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\0'))
 			str[i] -= 32;
-		write(1, &str[i++], 1);
+		write(1, &str[i], 1);
+		i++;
 	}
 }
 
