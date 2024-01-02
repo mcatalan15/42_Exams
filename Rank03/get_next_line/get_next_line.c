3a30a28f7b06d1cb6cdeb2444a9c27bb8638e8d8
mcatalan15 <marc.prat.2001@gmail.com>
2024-01-02 12:25:14 +0100

    Chgning gitignore

--- Rank03/get_next_line/get_next_line.c
+++ Rank03/get_next_line/get_next_line.c
@@ -103,3 +103,26 @@ char *get_next_line(int fd)
 	line[to_copy] = '\0';
 	return (line);
 }
+
+int main(void)
+{
+	int fd;
+	char *line;
+
+	fd = open("sample.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error opening file");
+		return 1;
+	}
+
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s\n", line);
+		free(line);
+	}
+
+	close(fd);
+
+	return 0;
+}
